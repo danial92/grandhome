7fa27928a341e460aa1667ca18f6e00683e6cc03
Danial <danialarshad02@gmail.com>
2021-04-21 23:58:51 -0400

    production commit

--- backend/app.js
+++ backend/app.js
@@ -26,18 +26,12 @@ app.use(require("./routes/uploadRoute"));
 const folder = path.resolve()
 app.use('/uploads', express.static(path.join(folder, '/uploads')))
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running..!!!')
-    })
-}
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')))
 
 // PORT
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3002
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 })
